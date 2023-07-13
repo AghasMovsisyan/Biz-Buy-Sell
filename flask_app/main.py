e33e03cdf07a955dd7bd5efde1de6caf0a4fac0d
Aghas <aghas.movsisyan3@gmail.com>
2023-07-13 12:44:46 +0400

    run black in main.py

--- flask_app/main.py
+++ flask_app/main.py
@@ -7,9 +7,11 @@ from models import Business, s
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
 def root_dir():
     """gives the directory name of the current script file."""
     return os.path.abspath(os.path.dirname(__file__))
+
 
 def get_file(filename):
     """get file"""
@@ -20,19 +22,20 @@ def get_file(filename):
         return str(exc)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def metrics():
-    """ A route handler for the root URL ("/") that serves the metrics page.
-        Retrieves the content of'index.html' file and returns it as a response with HTML mimetype"""
-    content = get_file('templates/index.html')
+    """A route handler for the root URL ("/") that serves the metrics page.
+    Retrieves the content of'index.html' file and returns it as a response with HTML mimetype
+    """
+    content = get_file("templates/index.html")
     return Response(content, mimetype="text/html")
 
 
-@app.route('/api/business', methods=['GET'])
+@app.route("/api/business", methods=["GET"])
 def get_business():
-    """Retrieves paginated items from the 'Business' collection ased on 'page' and 'limit' """
-    page = int(request.args.get('page', 1))
-    limit = int(request.args.get('limit', 10))
+    """Retrieves paginated items from the 'Business' collection ased on 'page' and 'limit'"""
+    page = int(request.args.get("page", 1))
+    limit = int(request.args.get("limit", 10))
     offset = (page - 1) * limit if page > 0 else 0
     paginated_items = s.query(Business).offset(offset).limit(limit).all()
 
@@ -41,7 +44,9 @@ def get_business():
     s.close()
 
     # Returns paginated items and total pages in a JSON response
-    result = jsonify(items=[item.json() for item in paginated_items], totalPages=total_pages)
+    result = jsonify(
+        items=[item.json() for item in paginated_items], totalPages=total_pages
+    )
     return result
 
 
@@ -52,8 +57,8 @@ def calculate_total_pages(limit):
     return total_pages
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
 def get_resource(path):
     """define a route handler for serving static resources from a specified directory"""
     mimetypes = {
@@ -68,5 +73,5 @@ def get_resource(path):
     return Response(content, mimetype=mimetype)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="localhost", port=9000, debug=True)
