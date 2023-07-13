2e466aab00580250507e19f8ae3dc2c005be6491
Aghas <aghas.movsisyan3@gmail.com>
2023-07-13 12:27:49 +0400

    black run in models.py

--- flask_app/models.py
+++ flask_app/models.py
@@ -18,8 +18,8 @@ database_host = os.getenv("DATABASE_HOST")
 database_port = os.getenv("DATABASE_PORT")
 database_name = os.getenv("DATABASE_NAME")
 
-database_uri = f"postgresql+psycopg2://{database_username}:{database_password}@{database_host}:{database_port}/{database_name}" # pylint: disable=line-too-long
-#Connecting to Postgres
+database_uri = f"postgresql+psycopg2://{database_username}:{database_password}@{database_host}:{database_port}/{database_name}"  # pylint: disable=line-too-long
+# Connecting to Postgres
 
 
 # pylint: disable=too-few-public-methods
@@ -29,7 +29,8 @@ class Base(DeclarativeBase):
 
 class User(Base):
     """class representing User"""
-    __tablename__ = 'User'
+
+    __tablename__ = "User"
     id = Column(Integer, primary_key=True)
     email = Column(String)
     password_hash = Column(String)
@@ -42,13 +43,15 @@ class User(Base):
 
     def json(self):
         """return self.{name}"""
-        return {'id': self.id,'email': self.email, 'salt': self.salt}
+        return {"id": self.id, "email": self.email, "salt": self.salt}
+
 
 class Business(Base):
     """class representing Busines"""
-    __tablename__ = 'Business'
+
+    __tablename__ = "Business"
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('User.id'))
+    user_id = Column(Integer, ForeignKey("User.id"))
     image_dir = Column(String)
     location = Column(String)
     property_type = Column(String)
@@ -60,12 +63,20 @@ class Business(Base):
 
     def json(self):
         """return self.{name}"""
-        return {'id': self.id, 'user_id': self.user_id, 'image_dir': self.image_dir,
-                'location': self.location, 'property_type': self.property_type, 'price': self.price, 
-                'year_built': self.year_built, 'size': self.size, 'name': self.name}
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "image_dir": self.image_dir,
+            "location": self.location,
+            "property_type": self.property_type,
+            "price": self.price,
+            "year_built": self.year_built,
+            "size": self.size,
+            "name": self.name,
+        }
 
 
-engine = create_engine(database_uri) #Creating a table
+engine = create_engine(database_uri)  # Creating a table
 Session = sessionmaker(bind=engine)
 
 Base.metadata.create_all(engine)
@@ -73,26 +84,26 @@ Base.metadata.create_all(engine)
 
 with engine.connect() as conn:
     Users = User(
-        id = 14,
-        email ='armansd@mail.ru',
-        password_hash = 'dada5f',
-        salt = 1,
-        first_name = 'dadf',
-        last_name = 'adaadhlfds',
-        tel_number = '+37496624514',
-        dalte_of_birth=datetime(516, 11, 18)
+        id=14,
+        email="armansd@mail.ru",
+        password_hash="dada5f",
+        salt=1,
+        first_name="dadf",
+        last_name="adaadhlfds",
+        tel_number="+37496624514",
+        dalte_of_birth=datetime(516, 11, 18),
     )
 
     Businesses = Business(
-        id = 14,
-        user_id = 14,
-        image_dir = '',
-        location = 'Komitas 1/8',
-        property_type = 'Cafe',
-        price = 150000,
-        year_built = '2012',
-        size = 2100,
-        name = 'Sava Super'
+        id=14,
+        user_id=14,
+        image_dir="",
+        location="Komitas 1/8",
+        property_type="Cafe",
+        price=150000,
+        year_built="2012",
+        size=2100,
+        name="Sava Super",
     )
 
 s = Session()
