a00d49e2041e665dc0c3e04ea4abc3d5b4a7aedf
Aghas <aghas.movsisyan3@gmail.com>
2023-08-08 16:00:43 +0400

    implement Detail view in Progress

--- flask_app/js/view.js
+++ flask_app/js/view.js
@@ -29,17 +29,22 @@ var cardDisplayModule = (function () {
                         <img class="imgv" src="${data.image_dir}">
                          ${isOwner ? '<button class="edit-button">Edit</button>' : ''}
                     </div>
-        
                 </div>
                 <div class="cardv-info">  
                     <ul>
                         <li><strong>Business Name:</strong> <span>${data.name}</span></li>
-                        <li><strong>Location:</strong> <span>${data.location}</span></li>
+                        <li><strong>Location:</strong> <span>${data.location}<img class="location" src="../logo/icons8-location-48.png"></span></li>
                         <li><strong>Price:</strong> <span>${data.price} <img class="dollar" src="../logo/free-icon-dollar-symbol-2150150.png"></span></li>
-                        <li><strong>Size:</strong> <span>${data.size}</span></li>
+                        <li><strong>Size:</strong> <span>${data.size}<img class="size" src="../logo/icons8-size-24.png "</span></li>
                         <li><strong>Telephone Number:</strong> <span>${data.tel_number} <img class="ico1" src="../logo/telephone-call.png"></span></li> 
                     </ul>
                 </div>
+                <div class="cardv-descript">
+                    <h2 class="decsribe-paragraph">Business Description</h2>
+                    <div class="cardv-description">
+                        <p>This tool rearranges the order of lines in the given textual data. It uses the Knuth method to do it, aka the Fisher and Yates order permutation algorithm. The behavior of the algorithm changes based on how many lines are shuffled at the same time. By default, it takes every single line in turn (1, 2, 3, …, n), generates a random number from 1 to n for it, which is the new position, and moves the line to this new place. It can also take several lines at once and move them together without splitting them apart. For example, if you enter the number 2 in the group size option, it will glue the 1st and 2nd lines together and move them together to </p>
+                    </div>
+                </div>  
                 `;
 
                 // Set the HTML content in the card-details element
