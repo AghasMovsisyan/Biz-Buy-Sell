af148f420e8c5c095e4ec58be2cd1c17336d9726
Aghas <aghas.movsisyan3@gmail.com>
2023-09-27 18:58:15 +0400

    add for price,size inputs field validation

--- flask_app/js/view.js
+++ flask_app/js/view.js
@@ -85,6 +85,7 @@ var cardDisplayModule = (function () {
                                             <div class="form-group" style="display: inline-block;">
                                                 <input type="text" class="form-control smaller-input" id="edit-size-${cardId}" value="${data.size}" style="width: 160px;">
                                             </div>
+                                            <div id="edit-size-error-${cardId}" class="text-danger" style="display: none;">Please enter a valid integer.</div>
                                         </form>
                                     </li>
 
@@ -96,6 +97,7 @@ var cardDisplayModule = (function () {
                                             <div class="form-group" style="display: inline-block;">
                                                 <input type="text" class="form-control smaller-input" id="edit-price-${cardId}" value="${data.price}" style="width: 160px;">
                                             </div>
+                                            <div id="edit-price-error-${cardId}" class="text-danger" style="display: none;">Please enter a valid integer.</div>
                                         </form>
                                     </li>
 
@@ -176,11 +178,7 @@ var cardDisplayModule = (function () {
                                     span.style.display = 'inline';
                                 });
 
-                                const editYearError = document.getElementById(`edit-year-error-${cardId}`);
-                                editYearError.style.display = 'none';
-                                const yearBuiltInput = document.getElementById(`edit-year-${cardId}`);
-                                yearBuiltInput.style.borderColor = ''; // Reset border color
-
+                                
                                 const saveButton = cardInfo.querySelector('.save-button');
                                 if (saveButton) {
                                     saveButton.style.display = 'none';
@@ -213,38 +211,45 @@ var cardDisplayModule = (function () {
 
                             const editYearError = document.getElementById(`edit-year-error-${cardId}`);
                             editYearError.style.display = 'none';
-                            const yearBuiltInput = document.getElementById(`edit-year-${cardId}`);
                             yearBuiltInput.style.borderColor = ''; // Reset border color
+
+                            const editPriceError = document.getElementById(`edit-price-error-${cardId}`);
+                            editPriceError.style.display = 'none';
+                            priceInput.style.borderColor = ''; // Reset border color
+
+                            const editSizeError = document.getElementById(`edit-size-error-${cardId}`); 
+                            editSizeError.style.display = 'none';
+                            sizeInput.borderColor = '';
 
                         }
                         
                         
-                        // Add an event listener to the "Year Built" input field for live validation
-                                                // Add an event listener to the "Year Built" input field for live validation
                         const yearBuiltInput = document.getElementById(`edit-year-${cardId}`);
+                        const priceInput = document.getElementById(`edit-price-${cardId}`);
+                        const sizeInput = document.getElementById(`edit-size-${cardId}`);
                         const saveButton = cardDetails.querySelector('.save-button');
-
+                        
                         function validateYearInput() {
                             const inputValue = yearBuiltInput.value;
-
+                        
                             if (inputValue.trim() === '') {
                                 // Input is empty
                                 const editYearError = document.getElementById(`edit-year-error-${cardId}`);
                                 editYearError.textContent = 'Year must not be empty'; // Set the error message
                                 editYearError.style.display = 'block';
                                 yearBuiltInput.style.borderColor = 'red'; // Highlight the input field
-
+                        
                                 // Disable the "Save" button
                                 saveButton.disabled = true;
                             } else if (/^\d+$/.test(inputValue)) {
                                 // Input is a valid integer
                                 const editedYear = parseInt(inputValue);
-
+                        
                                 // Reset error state if the input is valid
                                 const editYearError = document.getElementById(`edit-year-error-${cardId}`);
                                 editYearError.style.display = 'none';
                                 yearBuiltInput.style.borderColor = ''; // Reset border color
-
+                        
                                 // Enable the "Save" button
                                 saveButton.disabled = false;
                             } else {
@@ -253,17 +258,88 @@ var cardDisplayModule = (function () {
                                 editYearError.textContent = 'Please enter a valid integer'; // Set the error message
                                 editYearError.style.display = 'block';
                                 yearBuiltInput.style.borderColor = 'red'; // Highlight the input field
-
+                        
                                 // Disable the "Save" button
                                 saveButton.disabled = true;
                             }
                         }
+                        
+                        function validatePriceInput() {
+                            const inputValue = priceInput.value;
+                        
+                            if (inputValue.trim() === '') {
+                                // Input is empty
+                                const editPriceError = document.getElementById(`edit-price-error-${cardId}`);
+                                editPriceError.textContent = 'Price must not be empty'; // Set the error message
+                                editPriceError.style.display = 'block';
+                                priceInput.style.borderColor = 'red'; // Highlight the input field
+                        
+                                // Disable the "Save" button
+                                saveButton.disabled = true;
+                            } else if (/^\d+$/.test(inputValue)) {
+                                // Input is a valid integer
+                                const editedPrice = parseInt(inputValue);
+                        
+                                // Reset error state if the input is valid
+                                const editPriceError = document.getElementById(`edit-price-error-${cardId}`);
+                                editPriceError.style.display = 'none';
+                                priceInput.style.borderColor = ''; // Reset border color
+                        
+                                // Enable the "Save" button
+                                saveButton.disabled = false;
+                            } else {
+                                // Input is not a valid integer
+                                const editPriceError = document.getElementById(`edit-price-error-${cardId}`);
+                                editPriceError.textContent = 'Please enter a valid integer'; // Set the error message
+                                editPriceError.style.display = 'block';
+                                priceInput.style.borderColor = 'red'; // Highlight the input field
+                        
+                                // Disable the "Save" button
+                                saveButton.disabled = true;
+                            }
+                        }
+                        
+                        function validateSizeInputs() {
+
+
+                            const inputValue = sizeInput.value;
+                        
+                            if(inputValue.trim() === '') {
+                                // Input is empty
+                                const editSizeError = document.getElementById(`edit-size-error-${cardId}`);
+                                editSizeError.textContent = 'Size must not be empty';
+                                editSizeError.style.display = 'block';
+                                sizeInput.style.borderColor = 'red';
+                        
+                                saveButton.disabled = true;
+                            }else if (/^\d+$/.test(inputValue)) {
+                        
+                                const editedSize = parseInt(inputValue);
+                                const editSizeError = document.getElementById(`edit-size-error-${cardId}`);
+                                editSizeError.style.display = 'none';
+                                sizeInput.borderColor = '';
+                        
+                                saveButton.disabled = false;
+                            
+                            } else {
+                        
+                                const editSizeError = document.getElementById(`edit-size-error-${cardId}`);
+                                editSizeError.textContent = 'Please enter a valide integer';
+                                editSizeError.style.display = 'block';
+                                editSizeError.style.borderColor = 'red';
+                                
+                                saveButton.disabled = true;
+                        
+                            }
+                        }
 
                         yearBuiltInput.addEventListener('input', validateYearInput);
-
-                        // Initially validate the year input on page load
+                        priceInput.addEventListener('input', validatePriceInput);
+                        sizeInput.addEventListener('input', validateSizeInputs);
+                        // Initially validate both year and price inputs on page load
                         validateYearInput();
-
+                        validatePriceInput();
+                        validateSizeInputs();
 
 
 
